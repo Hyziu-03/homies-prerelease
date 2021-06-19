410461deb1c0a18234782f8203f9b8f29bc24201
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-06-19 16:13:25 +0200

    Applying logic to the menu's behavior

--- js/hamburger-menu-toggler.js
+++ js/hamburger-menu-toggler.js
@@ -1,10 +1,31 @@
 var hamburgerMenu = document.querySelector('.hamburger-menu');
-var navigation = document.querySelector('.navigation-list');
 var lines = document.querySelectorAll('.line');
+var navigationList = document.getElementById('navigation-list');
+
+var headerHeight = document.getElementById('header').clientHeight;
+var styles = '.navigation-list { display: flex; flex-direction: column; justify-content: center; align-items: center; position: absolute; top: 0; left: 16px; backdrop-filter: blur(5px); } .navigation-item { padding: 24px 0; position: relative; bottom: 0; margin: 5px; width: 90vw; text-align: center; background: #3B5672; } .navigation-list:first-child { margin-top: 100px !important; } .navigation-list:last-child { margin-bottom: 20px !important; }';
+var stylesheet = document.createElement('style');
+
+var isOpen = false;
 
 hamburgerMenu.addEventListener('click', function () {
-    navigation.classList.toggle('navigation-list-down');
-    lines.forEach(line => {
-        line.classList.toggle('active');
-    });
-})
+    if(isOpen == false) {
+        navigationList.style.marginTop = headerHeight + 'px';
+        stylesheet.innerText = styles;
+        document.head.appendChild(stylesheet);
+        lines.forEach(line => {
+            line.classList.toggle('active');
+        });
+        navigationList.style.visibility = 'visible';
+        
+        isOpen = true;
+    } else {
+        isOpen = false;
+        
+        navigationList.style.visibility = 'hidden';
+        lines.forEach(line => {
+            line.classList.remove('active');
+        });
+        document.head.removeChild(stylesheet);   
+    }
+});
--- js/illustration-height-setter.js
+++ js/illustration-height-setter.js
@@ -1,9 +1,7 @@
 var targettedHeight = document.getElementById('first-introductory-text').clientHeight;
 var illustration = document.getElementById('illustration-one');
 var secondIllustration = document.getElementById('illustration-two');
-var thirdIllustration = document.getElementById('illustration-three');
 var ratio = 0.25;
 
 illustration.style.height = targettedHeight * ratio + 'px';
 secondIllustration.style.height = targettedHeight * ratio + 'px';
-thirdIllustration.style.height = targettedHeight * ratio + 'px';

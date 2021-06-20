ccf765cdfdf60af2927c5ea5ab712ec3f89474d8
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-06-20 13:32:54 +0200

    Bug fixes

--- js/equal-button-width-setter.js
+++ js/equal-button-width-setter.js
@@ -1,4 +1,4 @@
 var targettedWidth = document.getElementById('wider-button').clientWidth;
 var narrowerButton = document.getElementById('narrower-button');
 var multiplier = 0.62;
-narrowerButton.style.width = targettedWidth * multiplier + 'px';
+narrowerButton.style.width = targettedWidth * multiplier + 'px !important';
